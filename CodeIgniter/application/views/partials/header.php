96e14d357d6b319568cbca9b1823f555441fccc4
Fecouto <felipe.tk@gmail.com>
2018-10-23 15:44:51 -0300

    reorganizado exibição do header

---
+++ CodeIgniter/application/views/partials/header.php
@@ -0,0 +1,22 @@
+<header>
+            
+            <div id="cabecalho">
+                <a href="index.php"><img src="assets/images/201220130617091943bike2.jpg" alt="Icone Bike" id="logo"> </a>
+                <h1>ENCONTRE A BIKE PARA O SEU ESTILO</h1>
+
+                <?php if ($hasSearch) { ?>
+                    tem busca
+                <?php } ?>
+
+            </div>
+        </header>
+        <nav>
+            <div id="navegador">
+                <ul id="nav">
+                    <ol>
+                        <a href="index.php">HOME</a>
+                        <a href="InserirAnuncio.php">INSERIR ANÚNCIO</a>
+                    </ol>
+                </ul>
+            <div>
+        </nav>
