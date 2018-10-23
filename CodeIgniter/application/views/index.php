1eb2353443776d713575adc07358cff323bfa65c
Fecouto <felipe.tk@gmail.com>
2018-10-23 14:35:39 -0300

    Reorganiza os templates

--- CodeIgniter/application/views/index.php
+++ CodeIgniter/application/views/index.php
@@ -1,40 +1,35 @@
-<!doctype html>
-<html>
-    <HEAD>
-           <title>Anúncios Bicicletas</title>
-           <meta charset="utf-8" />
-           <link rel="stylesheet" type="text/css" href="Estilo.css"/>
-    </HEAD>
-    <body id="corpo">
-        <div id="principal">
-            <header>
-                <div id="cabecalho">
-                    <a href="index.php"><img src="img/201220130617091943bike2.jpg" alt="Icone Bike" id="logo"> </a>
-                    <h1>ENCONTRE A BIKE PARA O SEU ESTILO</h1>
+<?php include 'partials/head.php'; ?>
 
-                </div>
-            </header>
-            <nav>
-                <div id="navegador">
-                    <ul id="nav">
-                        <ol>
-                            <a href="index.php">HOME</a>
-                            <a href="InserirAnuncio.php">INSERIR ANÚNCIO</a>
-                        </ol>
-                    </ul>
-                <div>
-            </nav>
-            <section>
-                <div id="secao">
-                    <p class = "estilo">Lorem ipsum blandit malesuada aenean nisl consectetur arcu ac feugiat, est libero id mi sed
-                        tristique sagittis. etiam inceptos massa platea velit ultricies facilisis vestibulum sed fermentum,
-                        vel curabitur etiam adipiscing nibh tempus nulla felis. hac netus torquent nam orci senectus netus dolor cursus, metus
-                        euismod eros placerat suspendisse luctus. </p>
-                     <p class = "estilo">Aptent sagittis nisi suspendisse cubilia adipiscing ut phasellus lacinia placerat risus, 
-                        mauris quisque accumsan bibendum sollicitudin bibendum mi ligula potenti varius, egestas aptent vestibulum hac proin vivamus tincidunt mollis nec.
-                        ante faucibus felis a ipsum viverra ornare nulla, pretium sapien senectus quisque hendrerit pellentesque, phasellus lobortis gravida ac ipsum tempor. </p>
-                </div>
-            </section>
-        </div>
-    </body>
-</html>
+    <div id="principal">
+        <header>
+            
+            <div id="cabecalho">
+                <a href="index.php"><img src="assets/images/201220130617091943bike2.jpg" alt="Icone Bike" id="logo"> </a>
+                <h1>ENCONTRE A BIKE PARA O SEU ESTILO</h1>
+
+            </div>
+        </header>
+        <nav>
+            <div id="navegador">
+                <ul id="nav">
+                    <ol>
+                        <a href="index.php">HOME</a>
+                        <a href="InserirAnuncio.php">INSERIR ANÚNCIO</a>
+                    </ol>
+                </ul>
+            <div>
+        </nav>
+        <section>
+            <div id="secao">
+                <p class = "estilo">Lorem ipsum blandit malesuada aenean nisl consectetur arcu ac feugiat, est libero id mi sed
+                    tristique sagittis. etiam inceptos massa platea velit ultricies facilisis vestibulum sed fermentum,
+                    vel curabitur etiam adipiscing nibh tempus nulla felis. hac netus torquent nam orci senectus netus dolor cursus, metus
+                    euismod eros placerat suspendisse luctus. </p>
+                    <p class = "estilo">Aptent sagittis nisi suspendisse cubilia adipiscing ut phasellus lacinia placerat risus, 
+                    mauris quisque accumsan bibendum sollicitudin bibendum mi ligula potenti varius, egestas aptent vestibulum hac proin vivamus tincidunt mollis nec.
+                    ante faucibus felis a ipsum viverra ornare nulla, pretium sapien senectus quisque hendrerit pellentesque, phasellus lobortis gravida ac ipsum tempor. </p>
+            </div>
+        </section>
+    </div>
+
+<?php include 'partials/foot.php'; ?>
---
+++ CodeIgniter/application/views/partials/head.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+    <HEAD>
+           <title>Anúncios Bicicletas</title>
+           <meta charset="utf-8" />
+           <link rel="stylesheet" type="text/css" href="assets/styles/estilo.css"/>
+    </HEAD>
+    <body id="corpo">
