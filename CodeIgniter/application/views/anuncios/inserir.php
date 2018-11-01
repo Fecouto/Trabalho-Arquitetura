805d4279160fa7535732b661f184d1d8e305d10f
Fecouto <felipe.tk@gmail.com>
2018-11-01 14:51:40 -0300

    Ajuste layout  view inserir anuncio

--- CodeIgniter/application/views/anuncios/inserir.php
+++ CodeIgniter/application/views/anuncios/inserir.php
@@ -11,11 +11,39 @@
                 <div id="secao">
                     <p class = "estilo2">Preencha os campos abaixo para inserir seu anúncio. </p>
                     <form action="/index.php/anuncios/salvar" method="POST" id="formulario">
-                        <label class="estilo2">Descrição<br></label>
-                        <input type="text" />
-                        <label class="estilo2"><br>Tipo<br></label>
-                        <input type="text" />
-                        <input type="submit" value ="Enviar"/>
+                        <div>
+                            <label class="estilo2">Descrição<br></label>
+                            <textarea name= "textarea" cols="40" rows="5" minlength="10" maxlength="200"></textarea>
+                        </div>
+                        <div>
+                            <label class="estilo2"><br>Tipo<br></label>
+                            <select id="select" name="tipo">
+                                <option>Peça</option>
+                                <option>Produto</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label class="estilo2"><br>Suspensão<br></label>
+                            <div id="selectsuspension">
+                                <ul>
+                                    <ol>
+                                        <label for="suspension" id="suspensao">Sim</label>
+                                        <input type="radio" id="susp" name="suspensao" value="sim">
+                                    
+                                        <label for="suspension" id="suspensao">Não</label>
+                                        <input type="radio" id="susp" name="suspensao" value="nao">
+                                    </ol>
+                                </ul>
+                            </div>
+                        </div>
+                        <div>
+                            <label class="estilo2"><br>Valor<br></label>
+                            <input type="text" />
+                        </div>
+                        <div>
+                            <label class="estilo2"><br><br></label>
+                            <input type="submit" value ="Salvar"/>
+                        </div>
                     </form>
                 </div>
             </section>
